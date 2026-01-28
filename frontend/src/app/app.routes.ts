e490fcbabbb6ecfd59ca6cb93eb4562b96787b49
Delta-44 <dalonso306@gmail.com>
2026-01-28 14:35:32 +0100

    finished loguin auth files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth/auth.guard';
+
+import { LoginPage } from './pages/login/login.page';
+import { RegisterPage } from './pages/register/register.page';
+import { AuthCallbackComponent } from './pages/auth-callback/auth-callback.component';
 import { StudentCrudComponent } from './pages/student-crud/student-crud.component';
 
 export const routes: Routes = [
-  { path: '', component: StudentCrudComponent }
+  { path: 'login', component: LoginPage },
+  { path: 'register', component: RegisterPage },
+  { path: 'auth/callback', component: AuthCallbackComponent }, // Google vuelve aquí
+  { path: '', component: StudentCrudComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: '' }
 ];
